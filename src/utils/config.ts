a665bcbea11d55fb3ef967f997737fd899dd46af
Julien Béranger <julien@strat.cc>
2023-05-19 18:20:32 +0000

    fix gnosisChiado

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,8 +1,8 @@
 import { ThemingProps } from '@chakra-ui/react'
-import { goerli } from '@wagmi/chains'
+import { gnosisChiado } from '@wagmi/chains'
 
-export const SITE_NAME = 'Another Star'
-export const SITE_DESCRIPTION = 'A Web3 app boilerplate built with Next.js, Chakra UI, Ethers, Wagmi and ConnectKit'
+export const SITE_NAME = 'gCFA App'
+export const SITE_DESCRIPTION = 'Deposit, withdraw or transfer your gCFA.'
 export const SITE_URL = 'https://w3hc.org'
 export const THEME_INITIAL_COLOR = 'dark'
 export const THEME_COLOR_SCHEME: ThemingProps['colorScheme'] = 'gray'
@@ -13,7 +13,7 @@ export const THEME_CONFIG = {
 export const SOCIAL_TWITTER = 'W3HC'
 export const SOCIAL_GITHUB = 'w3hc/nexth'
 
-export const ETH_CHAINS = [goerli]
+export const ETH_CHAINS = [gnosisChiado]
 export const alchemyId = process.env.NEXT_PUBLIC_ARBITRUM_ALCHEMY_ID
 
 export const SERVER_SESSION_SETTINGS = {
