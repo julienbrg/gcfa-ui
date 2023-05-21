e4dd961528b355e704be2b95eef614e9ef92e9c1
julienbrg <julien@strat.cc>
2023-05-21 15:50:38 +0200

    add pages

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Flex, useColorModeValue, Spacer, Heading } from '@chakra-ui/react'
+import { Flex, useColorModeValue, Spacer, Heading, Text } from '@chakra-ui/react'
 import { SITE_NAME } from '../../utils/config'
 import { LinkComponent } from './LinkComponent'
 import { ThemeSwitcher } from './ThemeSwitcher'
 import { ConnectKitButton } from 'connectkit'
+import Image from 'next/image'
 
 interface Props {
   className?: string
@@ -23,6 +24,22 @@ export function Header(props: Props) {
       <Spacer />
 
       <Flex alignItems="center" gap={4}>
+        <LinkComponent href="./dex">
+          <Heading as="h2" size="s">
+            DEX
+          </Heading>
+        </LinkComponent>
+        <LinkComponent href="./nfts">
+          <Heading as="h2" size="s">
+            NFTs
+          </Heading>
+        </LinkComponent>
+        <LinkComponent href="./dao">
+          <Heading as="h2" size="s">
+            DAO
+          </Heading>
+        </LinkComponent>
+        <Image priority height="30" width="30" alt="contract-image" src="./gnosis-owl.png" />
         <ConnectKitButton />
         <ThemeSwitcher />
       </Flex>
--- src/pages/dex/index.tsx
+++ src/pages/dex/index.tsx
@@ -1,15 +1,15 @@
 import { Heading, Button, useToast, FormControl, FormLabel, FormHelperText, Input, Text } from '@chakra-ui/react'
-import { Head } from '../components/layout/Head'
+import { Head } from '../../components/layout/Head'
 import Image from 'next/image'
-import { LinkComponent } from '../components/layout/LinkComponent'
+import { LinkComponent } from '../../components/layout/LinkComponent'
 import { useState, useEffect } from 'react'
 import { useFeeData, useSigner, useAccount, useBalance, useNetwork, useProvider } from 'wagmi'
 import { ethers } from 'ethers'
-import { GCFA_CONTRACT_ADDRESS, GCFA_CONTRACT_ABI, EURM_CONTRACT_ADDRESS, EURM_CONTRACT_ABI } from '../lib/consts'
+import { GCFA_CONTRACT_ADDRESS, GCFA_CONTRACT_ABI, EURM_CONTRACT_ADDRESS, EURM_CONTRACT_ABI } from '../../lib/consts'
 import useSound from 'use-sound' // https://www.joshwcomeau.com/react/announcing-use-sound-react-hook/
 const stevie = 'https://bafybeicxvrehw23nzkwjcxvsytimqj2wos7dhh4evrv5kscbbj6agilcsy.ipfs.w3s.link/another-star.mp3'
 
-export default function Home() {
+export default function Nfts() {
   const { data: signer } = useSigner()
 
   const cfa = new ethers.Contract(GCFA_CONTRACT_ADDRESS, GCFA_CONTRACT_ABI, signer)
@@ -397,218 +397,7 @@ export default function Home() {
       <Head />
 
       <main>
-        <p>Welcome to gCFA UI!</p>
-
-        {isDisconnected ? (
-          <>
-            <br />
-            <p>Please connect your wallet.</p>
-          </>
-        ) : (
-          <>
-            <br />
-            <p>
-              You can deposit your EUR to get the equivalent in gCFA, you can withdraw your gCFA and get your EUR back, and you also can do a simple
-              transfer.
-            </p>
-            <br />
-            <p>
-              Contract address: <strong>{GCFA_CONTRACT_ADDRESS}</strong>
-            </p>
-            <br />
-            <p>
-              Current total supply: <strong>{supply}</strong> gCFA
-            </p>
-            <br />
-            {cfaBal || eurBal ? (
-              <>
-                <p>
-                  You&apos;re connected to <strong>{network.chain?.name}</strong> and your wallet currently holds
-                  <strong> {userBal}</strong>, <strong>{cfaBal.toFixed(0)}</strong> gCFA, and <strong>{eurBal.toFixed(2)}</strong> EUR.{' '}
-                </p>
-                <br />
-              </>
-            ) : (
-              <>
-                <p>
-                  You&apos;re connected to <strong>{network.chain?.name}</strong>{' '}
-                  <Button size="xs" mr={3} mb={3} colorScheme="blue" variant="outline" onClick={() => getBalances()}>
-                    Get balances
-                  </Button>
-                </p>
-              </>
-            )}
-          </>
-        )}
-
-        <br />
-        {!loadingFaucet ? (
-          <Button mr={3} mb={3} colorScheme="green" variant="outline" onClick={getFreeMoney}>
-            Get some free xDAI
-          </Button>
-        ) : (
-          <Button mr={3} mb={3} isLoading colorScheme="green" loadingText="Cashing in" variant="outline">
-            Cashing in
-          </Button>
-        )}
-        {faucetTxLink ? (
-          <>
-            <br />
-            <Text fontSize="12px" color="#45a2f8">
-              <LinkComponent target="blank" href={faucetTxLink}>
-                View your faucet tx on Etherscan: <strong>{faucetTxLink}</strong>
-              </LinkComponent>
-            </Text>
-          </>
-        ) : (
-          <>
-            <br />
-          </>
-        )}
-        <br />
-
-        <FormControl>
-          <FormLabel>Mint EUR</FormLabel>
-          <Input value={eurAmount} type="number" onChange={(e) => setEurAmount(e.target.value)} placeholder="Proposal title" />
-          <FormHelperText>How many euros do you want to mint?</FormHelperText>
-
-          <br />
-          {!loadingMint ? (
-            <Button mr={3} mb={3} colorScheme="green" variant="outline" onClick={mint}>
-              Mint EUR
-            </Button>
-          ) : (
-            <Button mr={3} mb={3} isLoading colorScheme="green" loadingText="Minting" variant="outline">
-              Minting
-            </Button>
-          )}
-          {mintTxLink ? (
-            <>
-              <br />
-              <Text fontSize="12px" color="#45a2f8">
-                <LinkComponent target="blank" href={mintTxLink}>
-                  View your mint tx on Etherscan: <strong>{mintTxLink}</strong>
-                </LinkComponent>
-              </Text>
-            </>
-          ) : (
-            <>
-              <br />
-            </>
-          )}
-        </FormControl>
-        <br />
-        <FormControl>
-          <FormLabel>Deposit</FormLabel>
-          <Input value={depositAmount} onChange={(e) => setDepositAmount(e.target.value)} placeholder="Proposal title" />
-          <FormHelperText>How many euros do you want to deposit?</FormHelperText>
-
-          <br />
-          {!loadingDeposit ? (
-            <Button mr={3} mb={3} colorScheme="green" variant="outline" onClick={deposit}>
-              Deposit
-            </Button>
-          ) : (
-            <Button mr={3} mb={3} isLoading colorScheme="green" loadingText="Depositing" variant="outline">
-              Depositing
-            </Button>
-          )}
-          {depositTxLink ? (
-            <>
-              <br />
-              <Text fontSize="12px" color="#45a2f8">
-                <LinkComponent target="blank" href={depositTxLink}>
-                  View your deposit tx on Etherscan: <strong>{depositTxLink}</strong>
-                </LinkComponent>
-              </Text>
-            </>
-          ) : (
-            <>
-              <br />
-            </>
-          )}
-        </FormControl>
-
-        <br />
-
-        <FormControl>
-          <FormLabel>Withdraw</FormLabel>
-          <Input value={amountToWithdraw} onChange={(e) => setAmountToWithdraw(e.target.value)} placeholder="Proposal title" />
-          <FormHelperText>How many gCFA do you want to withdraw?</FormHelperText>
-
-          <br />
-          {!loadingWithdraw ? (
-            <Button mr={3} mb={3} colorScheme="green" variant="outline" onClick={withdraw}>
-              Withdraw
-            </Button>
-          ) : (
-            <Button mr={3} mb={3} isLoading colorScheme="green" loadingText="Withdrawing" variant="outline">
-              Withdrawing
-            </Button>
-          )}
-          {withdrawTxLink ? (
-            <>
-              <br />
-              <Text fontSize="12px" color="#45a2f8">
-                <LinkComponent target="blank" href={withdrawTxLink}>
-                  View your withdraw tx on Etherscan: <strong>{withdrawTxLink}</strong>
-                </LinkComponent>
-              </Text>
-            </>
-          ) : (
-            <>
-              <br />
-            </>
-          )}
-        </FormControl>
-
-        <br />
-        <FormControl>
-          <FormLabel>Transfer gCFA</FormLabel>
-          <Input value={recipientAddress} onChange={(e) => setRecipientAddress(e.target.value)} />
-          <FormHelperText>What&apos;s the recipent address?</FormHelperText>
-          <br />
-          <Input value={transferAmount} onChange={(e) => setTransferAmount(e.target.value)} />
-          <FormHelperText>How many gCFA do you want to transfer?</FormHelperText>
-          <br />
-          {!loadingTransfer ? (
-            <Button mr={3} mb={3} colorScheme="green" variant="outline" onClick={transfer}>
-              Transfer
-            </Button>
-          ) : (
-            <Button mr={3} mb={3} isLoading colorScheme="green" loadingText="Transferring" variant="outline">
-              Transferring
-            </Button>
-          )}
-          {transferTxLink ? (
-            <>
-              <br />
-              <Text fontSize="12px" color="#45a2f8">
-                <LinkComponent target="blank" href={transferTxLink}>
-                  View your transfer tx on Etherscan: <strong>{transferTxLink}</strong>
-                </LinkComponent>
-              </Text>
-            </>
-          ) : (
-            <>
-              <br />
-            </>
-          )}
-        </FormControl>
-
-        <br />
-        {/* {txLink && (
-          <Button colorScheme="red" variant="outline" onClick={() => stop()}>
-            Stop the music
-          </Button>
-        )} */}
-        <Image
-          priority
-          height="800"
-          width="1000"
-          alt="contract-image"
-          src="https://bafybeidfcsm7moglsy4sng57jdwmnc4nw3p5tjheqm6vxk3ty65owrfyk4.ipfs.w3s.link/gcfa-code.png"
-        />
+        <p>Hello DEX</p>
       </main>
     </>
   )
