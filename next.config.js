daa150eaa7ba3898c1147d850bccb5c3535806bb
julienbrg <julien@strat.cc>
2023-05-27 06:36:12 +0200

    fix remotePatterns

--- next.config.js
+++ next.config.js
@@ -11,18 +11,18 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'example.com',
         port: '',
-        output: 'export',
-        trailingSlash: true,
-        exportPathMap: function () {
-          return {
-            '/': { page: '/' },
-            '/dex': { page: '/dex' },
-            '/nfts': { page: '/nfts' },
-            '/dao': { page: '/dao' },
-          }
-        },
       },
     ],
+  },
+  output: 'export',
+  trailingSlash: true,
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+      '/dex': { page: '/dex' },
+      '/nfts': { page: '/nfts' },
+      '/dao': { page: '/dao' },
+    }
   },
 }
 
